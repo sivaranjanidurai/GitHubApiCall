18815accfa8db29c0d125580a7e9517d83a84a54
sivaranjani <ranjanicse1997@gmail.com>
2024-08-21 21:36:40 +0800

    'commit'

--- src/main/java/com/github/repo/application/port/input/GitHubRepoUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.github.repo.application.port.input;
-
-import com.github.repo.application.dto.GitHubRepoInfoDto;
-
-public interface GitHubRepoUseCase
-{
-    public GitHubRepoInfoDto getRepoInfoThroughAPIorCache(String owner, String repositoryName);
-}
--- src/main/java/com/github/repo/infrastructure/exception/GitRepoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.repo.infrastructure.exception;
-
-public class GitRepoNotFoundException  extends RuntimeException {
-    public GitRepoNotFoundException(String message) {
-        super(message);
-    }
-}
